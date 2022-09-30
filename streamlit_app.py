423e5e167345a72cf88ebf94a5863c717c12cb5e
sai-kumar-aalla <114729346+sai-kumar-aalla@users.noreply.github.com>
2022-09-30 13:54:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
 
 streamlit . title('my parent new healthy diner')
+streamlit.header('breakfast menu')
+streamlit.text('omega 3 & blueberry Oatmeal')
+streamlit . text ('kale,spinach & rocket smoothie')
+stream . text ('hard-boiled free-range egg')
 
